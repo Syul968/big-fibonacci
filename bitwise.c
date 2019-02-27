b0c3f5a693c9692a3c537fafc1a8c8114b513c1f
Syul <syul.pgn@gmail.com>
2019-02-26 23:04:59 -0600

    struct defined; and, or, xor implemented

---
+++ bitwise.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct bignum {
+	int bytes;
+	char *num;
+} bignum;
+
+int max(const int a, const int b) {
+	return (a > b) ? a : b;
+}
+
+int min(const int a, const int b) {
+	return (a < b) ? a : b;
+}
+
+void printCharBinary(const char c) {
+	for(int i = 1; i <= 8; i++)
+		printf("%d", (c >> (8 - i)) & 1);
+}
+
+void printBignum(const bignum bignum) {
+	for(int i = 1; i <= bignum.bytes; i++)
+		printCharBinary(bignum.num[bignum.bytes - i]);
+	printf("\n");
+}
+
+bignum and(const bignum a, const bignum b) {
+	bignum result;
+	result.bytes = max(a.bytes, b.bytes);
+	result.num = (char *)calloc(result.bytes, sizeof(char));
+	int minBytes = min(a.bytes, b.bytes);
+	
+	int i;
+	for(i = 0; i < minBytes; i++)
+		result.num[i] = a.num[i] & b.num[i];
+	while(i++ < result.bytes)
+		result.num[i] = 0;
+	
+	return result;
+}
+
+bignum or(const bignum a, const bignum b) {
+	bignum result;
+	result.bytes = max(a.bytes, b.bytes);
+	result.num = (char *)calloc(result.bytes, sizeof(char));
+	int minBytes = min(a.bytes, b.bytes);
+	
+	int i;
+	for(i = 0; i < minBytes; i++)
+		result.num[i] = a.num[i] | b.num[i];
+	while(i < a.bytes) {
+		result.num[i] = 0 | a.num[i];
+		i++;
+	}
+	while(i < b.bytes) {
+		result.num[i] = 0 | b.num[i];
+		i++;
+	}
+	
+	return result;
+}
+
+bignum xor(const bignum a, const bignum b) {
+	bignum result;
+	result.bytes = max(a.bytes, b.bytes);
+	result.num = (char *)calloc(result.bytes, sizeof(char));
+	int minBytes = min(a.bytes, b.bytes);
+	
+	int i;
+	for(i = 0; i < minBytes; i++)
+		result.num[i] = a.num[i] ^ b.num[i];
+	while(i < a.bytes) {
+		result.num[i] = 0 ^ a.num[i];
+		i++;
+	}
+	while(i < b.bytes) {
+		result.num[i] = 0 ^ b.num[i];
+		i++;
+	}
+	
+	return result;
+}
+
+int main(int argc, char *argv[]) {
+	bignum a;
+	a.bytes = 2;
+	a.num = (char *)calloc(a.bytes, sizeof(char));
+	a.num[0] = 18;
+	a.num[1] = 7;
+	
+	bignum b;
+	b.bytes = 1;
+	b.num = (char *)calloc(b.bytes, sizeof(char));
+	b.num[0] = 22;
+	
+	bignum c = xor(a, b);
+	
+	printBignum(c);
+}
